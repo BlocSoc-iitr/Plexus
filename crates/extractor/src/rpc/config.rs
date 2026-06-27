97c5fc1ee5beeaeb3d0ab64a0b2b4735d0322b80
PhAnToMxSD <aradhya.gzb08@gmail.com>
2026-06-28 02:25:46 +0530

    feat: added ClientConfig & suitable with/without url setup methods

--- crates/extractor/src/rpc/config.rs
+++ crates/extractor/src/rpc/config.rs
@@ -4,7 +4,7 @@ use std::time::Duration;
 
 use crate::rpc::config::JitterStrategy::Full;
 
-enum JitterStrategy {Full, Equal}
+enum JitterStrategy {Full} //Jitter_Strategy enum - defauts to Full jitter for better spread
 
 pub struct RetryConfig {
     pub max_attemps: u8,
@@ -13,14 +13,42 @@ pub struct RetryConfig {
     pub jitter: JitterStrategy
 }
 
+pub struct ClientConfig {
+    pub url: String,
+    pub max_concurrency: u32,
+    pub retry_config: RetryConfig
+}
+
 impl RetryConfig {
-    //Default settings for a RetryConfig
+    //Default setting for a RetryConfig
     pub fn default() -> Self {
         let def = RetryConfig {
             max_attemps: 3,
             base_delay: Duration::from_millis(100),
             max_delay: Duration::from_secs(2),
             jitter: JitterStrategy::Full
+        };
+        def
+    }
+}
+
+impl ClientConfig {
+    //Default setting for a ClientConfig
+    pub fn default() -> Self {
+        let def = ClientConfig {
+            url: String::from("http://localhost:8545"),
+            max_concurrency: 20,
+            retry_config: RetryConfig::default()
+        };
+        def
+    }
+
+    //ClientConfig setup with a url supplied by the client
+    pub fn new_with_endpoint(url: &String) -> Self {
+        let def = ClientConfig {
+            url: url.clone(),
+            max_concurrency: 20,
+            retry_config: RetryConfig::default()
         };
         def
     }
