ddef16cbe5d46409ce906af026bb0df085c006ca
PhAnToMxSD <aradhya.gzb08@gmail.com>
2026-06-29 04:37:44 +0530

    refactor: drop JitterStrategy, rename constructor functions to avoid impl clashes, add timeout and default concurrency with tests

--- crates/extractor/src/rpc/config.rs
+++ crates/extractor/src/rpc/config.rs
@@ -1,45 +1,40 @@
-#![allow(dead_code)]
-
 use std::time::Duration;
 
-#[derive(PartialEq, Debug)]
-pub enum JitterStrategy {Full} //Jitter_Strategy enum - defauts to Full jitter for better spread
-
+#[derive(Debug)]
 pub struct RetryConfig {
     pub max_attempts: u8,
     pub base_delay: Duration,
     pub max_delay: Duration,
-    pub jitter: JitterStrategy
 }
 
+#[derive(Debug)]
 pub struct ClientConfig {
     pub url: String,
     pub max_concurrency: u32,
-    pub retry_config: RetryConfig
+    pub retry_config: RetryConfig,
+    pub timeout: Duration,
 }
 
 impl RetryConfig {
     //Default setting for a RetryConfig
-    pub fn default() -> Self {
-        let def = RetryConfig {
+    pub fn new() -> Self {
+        RetryConfig {
             max_attempts: 3,
             base_delay: Duration::from_millis(100),
             max_delay: Duration::from_secs(2),
-            jitter: JitterStrategy::Full
-        };
-        def
+        }
     }
 }
 
 impl ClientConfig {
     //ClientConfig setup with a url supplied by the user
-    pub fn default_with_endpoint(url: &String) -> Self {
-        let def = ClientConfig {
-            url: url.clone(),
-            max_concurrency: 20,
-            retry_config: RetryConfig::default()
-        };
-        def
+    pub fn new_with_endpoint(url: &str) -> Self {
+        ClientConfig {
+            url: url.to_string(),
+            max_concurrency: 10,
+            retry_config: RetryConfig::new(),
+            timeout: Duration::from_secs(5),
+        }
     }
 }
 
@@ -50,14 +45,14 @@ mod test {
     #[test]
     fn clientconfig_url_with_default_returns_correct_params() {
         let url = String::from("https://ethereum-rpc.publicnode.com");
-        let client_config = ClientConfig::default_with_endpoint(&url);
+        let client_config = ClientConfig::new_with_endpoint(&url);
 
         assert_eq!(client_config.url, url);
 
-        //If these basic params are correct, other params will also be 
-        //correct by default. 
-        assert_eq!(client_config.retry_config.max_attempts,3);
-        assert_eq!(client_config.max_concurrency, 20);
-        assert_eq!(client_config.retry_config.jitter, JitterStrategy::Full);
+        //If these basic params are correct, other params will also be
+        //correct by default.
+        assert_eq!(client_config.retry_config.max_attempts, 3);
+        assert_eq!(client_config.max_concurrency, 10);
+        assert_eq!(client_config.timeout, Duration::from_secs(5));
     }
 }
